f8aa51b2b78d1cd765a76eead466c3ded0955f57
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-08-24 18:40:33 -0500

    Navigation controller updates for Stage 2, plus starting image work for Stage 3.

--- Algorhythm/PlaylistDetailViewController.swift
+++ Algorhythm/PlaylistDetailViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class PlaylistDetailViewController: UIViewController {
 
+    
+    @IBOutlet weak var buttonPressLabel: UILabel!
+    var segueLabelText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        buttonPressLabel.text = segueLabelText
     }
 
     override func didReceiveMemoryWarning() {
--- Algorhythm/PlaylistMasterViewController.swift
+++ Algorhythm/PlaylistMasterViewController.swift
@@ -21,6 +21,15 @@ class PlaylistMasterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showPlaylistDetail" {
+            // destinationViewController accesses the VC we're transitioning to through the segue; this returns a VC that has a type of AnyObject
+            //
+            let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+            playlistDetailController.segueLabelText = "Yay! You pressed the button!"
+        }
+    }
 
 }
 
