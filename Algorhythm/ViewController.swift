587afbb69c7595be8ae72087f7beeb71a3950aaf
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-08-23 19:27:25 -0500

    Initial Commit

---
+++ Algorhythm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorhythm
+//
+//  Created by Sarah Wheeler on 8/23/15.
+//  Copyright (c) 2015 Sarah Wheeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
