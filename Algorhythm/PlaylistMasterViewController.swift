73ab31c297265a0561a91bb6bc2583b816403111
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-09-04 17:22:37 -0500

    Fixes error in master view that was crashing the app.

--- Algorhythm/PlaylistMasterViewController.swift
+++ Algorhythm/PlaylistMasterViewController.swift
@@ -5,13 +5,11 @@
 //  Created by Sarah Wheeler on 8/23/15.
 //  Copyright (c) 2015 Sarah Wheeler. All rights reserved.
 //
-
 import UIKit
 
 class PlaylistMasterViewController: UIViewController {
     
     var playlistsArray: [UIImageView] = []
-    
     @IBOutlet weak var playlistImageView0: UIImageView!
     @IBOutlet weak var playlistImageView1: UIImageView!
     @IBOutlet weak var playlistImageView2: UIImageView!
@@ -19,21 +17,21 @@ class PlaylistMasterViewController: UIViewController {
     @IBOutlet weak var playlistImageView4: UIImageView!
     @IBOutlet weak var playlistImageView5: UIImageView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        playlistsArray += [playlistImageView0, playlistImageView1, playlistImageView2, playlistImageView3, playlistImageView3, playlistImageView4, playlistImageView5]
+        playlistsArray += [playlistImageView0, playlistImageView1, playlistImageView2, playlistImageView3, playlistImageView4, playlistImageView5]
         
         for index in 0..<playlistsArray.count {
             let playlist = Playlist(index: index)
             let playlistImageView = playlistsArray[index]
+            
             playlistImageView.image = playlist.icon
             playlistImageView.backgroundColor = playlist.backgroundColor
         }
-        
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -41,19 +39,19 @@ class PlaylistMasterViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showPlaylistDetailSegue" {
-            let playlistImageView = sender!.view as! UIImageView
             
+            let playlistImageView = sender!.view as! UIImageView
             if let index = find(playlistsArray, playlistImageView) {
-                // destinationViewController accesses the VC we're transitioning to through the segue; this returns a VC that has a type of AnyObject
                 let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
                 playlistDetailController.playlist = Playlist(index: index)
             }
+            
+            
         }
     }
-
+    
     @IBAction func showPlaylistDetail(sender: AnyObject) {
         performSegueWithIdentifier("showPlaylistDetailSegue", sender: sender)
-        
     }
+    
 }
-
